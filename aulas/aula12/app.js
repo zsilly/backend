388897489e95822e85a24b0d9e18b874c1c6f1bc
andresantos <andre.santos6@iesb.edu.br>
2025-11-06 10:47:46 -0300

    documentacao

---
+++ aulas/aula12/app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const apidocsRouter = require('./routes/apdocs');
+const app = express();
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use('/spi-docs',apidocsRouter);
+module.exports = app;
